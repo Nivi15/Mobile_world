1f4a3b5bc75be1cd221c7803c875f01fcca7b557
Nivi15 <86653469+Nivi15@users.noreply.github.com>
2022-10-12 09:11:24 +0530

    Update SignIn.java

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,28 +1,87 @@
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
 public class SignIn {
 	
-    @Test
+	@Test(description="user can signin with valid username and password")
     public void signIn() {
-	// TODO Auto-generated method stub
-	System.setProperty("webdriver.chrome.driver","C:/Users/nivethaa.e/Downloads/chromedriver/chromedriver.exe");
-	WebDriver driver = new ChromeDriver();
-	driver.get("https://mobileworld.banyanpro.com/");
-	LandingPage ob = new LandingPage(driver);
-	ob.SignIn("nivi05@gmail.com", "Passwd12");
-	ob.SignIn("ni", "Pswd23");
-	ob.SignIn("nivisha1@gmail.com", "Ps" );
-	ob.SignIn("nivi.e@gmail.com", "Password1234");
+        System.setProperty("webdriver.chrome.driver","C:/Users/nivethaa.e/Downloads/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qualicoach.org/mwapp/index.html");
+        LandingPage ob= new LandingPage(driver);
+        ob.SignIn("Nivethaa","Nivi15");
+        driver.quit();
 	}
-	}
-
+	
+	@Test(description="user cant signin by leaving username field empty")
+    public void Demo1() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/nivethaa.e/Downloads/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qualicoach.org/mwapp/index.html");
+        LandingPage ob= new LandingPage(driver);
+        ob.SignIn(" ","nivi15");
+        String actual="driver.getCurrentURL";
+        String expected="https://qualicoach.org/mwapp/sign.html";
+        Assert.assertEquals(actual, expected);
+        driver.quit();
+        }
+	
+	@Test(description="user cant signin by leaving both username and password field empty" )
+    public void Demo2() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/nivethaa.e/Downloads/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qualicoach.org/mwapp/index.html");
+        LandingPage ob= new LandingPage(driver);
+        ob.SignIn(" "," ");
+        String actual="driver.getCurrentURL";
+        String expected="https://qualicoach.org/mwapp/sign.html";
+        Assert.assertEquals(actual, expected);
+        driver.quit();
+        }
+	
+	@Test(description="user cant signin by entering less than 3 characters in username")
+    public void Demo3() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/nivethaa.e/Downloads/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qualicoach.org/mwapp/index.html");
+        LandingPage ob= new LandingPage(driver);
+        ob.SignIn("Ni","Nivi153 ");
+        String actual="driver.getCurrentURL";
+        String expected="https://qualicoach.org/mwapp/sign.html";
+        Assert.assertEquals(actual, expected);
+        driver.quit();
+        }
+	
+	@Test(description="user cant signin by entering more than 20 characters in username")
+    public void Demo4() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/nivethaa.e/Downloads/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qualicoach.org/mwapp/index.html");
+        LandingPage ob= new LandingPage(driver);
+        ob.SignIn("NivethaaElumalaishaaheidii","nivi123 ");
+        String actual="driver.getCurrentURL";
+        String expected="https://qualicoach.org/mwapp/sign.html";
+        Assert.assertEquals(actual, expected);
+        driver.quit();
+        }
+	
+	
+    @Test(description="user cant signin by entering special characters in password")
+    public void Demo5() {
+        System.setProperty("webdriver.chrome.driver","C:/Users/nivethaa.e/Downloads/chromedriver/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://qualicoach.org/mwapp/index.html");
+        LandingPage ob= new LandingPage(driver);
+        ob.SignIn("nivethaa","nivi!@");
+        String actual="driver.getCurrentURL";
+        String expected="https://qualicoach.org/mwapp/sign.html";
+        Assert.assertEquals(actual, expected);
+        driver.quit();
+    }
+}
 
 	
 	
